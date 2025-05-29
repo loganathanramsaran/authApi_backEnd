6026925dae67cf0b391c1fc1b7320477ff315410
loganathanramsaran <loguflower3@gmail.com>
2025-05-29 17:06:50 +0530

    mongoose connect

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
@@ -6,6 +8,18 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res) => {
   res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
 });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected (server.js)');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error(server.js):', err.message);
+    process.exit(1);
+  });
 
 
 app.listen(PORT, () => {
