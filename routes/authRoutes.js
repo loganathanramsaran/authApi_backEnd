0e8aa8cdc69b0e80d5d20e9da6c0de7cf9512955
loganathanramsaran <loguflower3@gmail.com>
2025-05-29 16:09:01 +0530

    delete user in auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getProfile } = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
+const { deleteUser } = require('../controllers/userController');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile', protect, getProfile);
-
+router.get('/profile', getProfile);
+router.delete('/users/:id', deleteUser);
 module.exports = router;
