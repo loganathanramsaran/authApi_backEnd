4222723f2e80225b07bdc2325fceaa58e7cbbb63
loganathanramsaran <loguflower3@gmail.com>
2025-05-29 17:31:56 +0530

    path modify

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/auth/users', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
